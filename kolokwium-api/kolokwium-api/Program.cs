a1282c527c1c08a526de87c8eb00eb6784193ad7
Jakub Chodziński <xapixowy@gmail.com>
2024-06-12 20:22:03 +0200

    Program.cs changes

--- kolokwium-api/kolokwium-api/Program.cs
+++ kolokwium-api/kolokwium-api/Program.cs
@@ -6,6 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 // TODO: To trzeba dodać
 builder.Services.AddCompanyServices();
 builder.Services.AddControllers();
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.Local.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
